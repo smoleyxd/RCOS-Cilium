d6bf1b49d8601b1c29863d68f5ce86f7101cb08a
smoleyxd <bsf003@yahoo.com>
2024-03-27 00:29:21 -0400

    Added helm to project structure + added basic multicast testing notes & scripts

---
+++ testing/sub.py
@@ -0,0 +1,17 @@
+import socket
+import struct
+
+MCAST_GRP = '224.1.1.1'
+MCAST_PORT = 5007
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+sock.bind(('', MCAST_PORT))  # Bind to all interfaces on the MCAST_PORT
+
+mreq = struct.pack("4sl", socket.inet_aton(MCAST_GRP), socket.INADDR_ANY)
+sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
+
+while True:
+    print(f"Waiting for multicast message...")
+    data, addr = sock.recvfrom(1024)
+    print(f"Received message: {data} from {addr}")
