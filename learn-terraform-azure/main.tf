706f1e8dae9e051f094094e8c40ffaf4c122f58e
bnavac <navacb@rpi.edu>
2024-06-11 15:11:52 -0400

    removed duplicated providers from main

--- learn-terraform-azure/main.tf
+++ learn-terraform-azure/main.tf
@@ -1,22 +1,5 @@
 # Configure the Azure provider
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.67.0"
-    }
-  }
 
-  required_version = ">= 1.1.0"
-}
-
-provider "azurerm" {
-    client_id = var.CLIENT_ID
-    client_secret = var.CLIENT_SECRET
-    subscription_id = var.SUBSCRIPTION_ID
-    tenant_id = var.TENANT_ID
-    features {}
-}
 
 data "azurerm_resource_group" "rg" {
   name     = "RCOS-Cilium_group"
--- learn-terraform-azure/variables.tf
+++ learn-terraform-azure/variables.tf
@@ -1,12 +1,16 @@
 variable "CLIENT_SECRET"{
     type = string
+    description = "Password for the azure cli user"
 }
 variable "CLIENT_ID"{
     type = string
+    description = "appId for the azure cli user"
 }
 variable "TENANT_ID"{
     type = string
+    description = "tenant id for the rcos-cilium_group resource group"
 }
 variable "SUBSCRIPTION_ID"{
     type = string
+    description = "subscription id for the rcos-cilium_group resource group"
 }
