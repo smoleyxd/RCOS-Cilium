471a1d3ab8e656686c60521c6a5ed05f60dd0d4b
bnavac <navacb@rpi.edu>
2024-06-14 14:13:57 -0400

    Moved aks, added helm

--- learn-terraform-azure/main.tf
+++ learn-terraform-azure/main.tf
@@ -12,24 +12,4 @@ resource "azurerm_virtual_network" "vnet"{
         resource_group_name = data.azurerm_resource_group.rg.name
 }
 
-resource "azurerm_kubernetes_cluster" "default" {
-    name = "test-aks"
-    location = data.azurerm_resource_group.rg.location
-    resource_group_name = data.azurerm_resource_group.rg.name
-    dns_prefix = "test-k8s"
-    kubernetes_version = "1.29.4"
-    default_node_pool{
-        name = "default"
-        node_count = 1
-        vm_size = "Standard_A2_v2"
-        os_disk_size_gb = 30
-    }
-    service_principal{
-        client_id = var.CLIENT_ID
-        client_secret = var.CLIENT_SECRET
-    }
-    role_based_access_control_enabled = true
-    tags = {
-        environment = "Demo"
-    }
-}
+
--- learn-terraform-azure/versions.tf
+++ learn-terraform-azure/versions.tf
@@ -4,6 +4,10 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "~> 3.67.0"
     }
+    helm = {
+        source = "hashicorp/helm"
+        version = "~> 2.0"
+    }
   }
 
   required_version = ">= 1.1.0"
