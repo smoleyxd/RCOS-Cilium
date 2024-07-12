d36dfb9efde581e76355e6a3e2c1ce60554c4903
bnavac <navacb@rpi.edu>
2024-07-12 14:18:49 -0400

    Added system assignment

--- main.tf
+++ main.tf
@@ -4,12 +4,23 @@ data "azurerm_resource_group" "rg" {
   name = "RCOS-Cilium_group"
 }
 
+data "azurerm_subscription" "current"{}
+/*
 resource "azurerm_user_assigned_identity" "monitor"{
     location = var.RESOURCE_GROUP_LOCATION
     name = "monitor"
     resource_group_name = data.azurerm_resource_group.rg.name
 }
+data "azurerm_role_definition" "contributor"{
+    name = "Contributor"
+}
 
+resource "azurerm_role_assignment" "assignment"{
+    scope = data.azurerm_subscription.current.id
+    role_definition_id = "${data.azurerm_subscription.current.id}${data.azurerm_role_definition.contributor.id}"
+    principal_id = azurerm_kubernetes_cluster.default.identity[0].principal_id
+}
+*/
 resource "azurerm_virtual_network" "vnet" {
   name                = "test-vnet"
   address_space       = ["10.0.0.0/8"]
@@ -37,7 +48,9 @@ resource "azurerm_kubernetes_cluster" "default" {
   resource_group_name = data.azurerm_resource_group.rg.name
   dns_prefix          = "test-cilium-k8s"
   kubernetes_version  = "1.30"
-
+  identity{
+    type = "SystemAssigned"
+  }
   default_node_pool {
     name            = "default"
     node_count      = 2
@@ -46,10 +59,12 @@ resource "azurerm_kubernetes_cluster" "default" {
     vnet_subnet_id  = azurerm_subnet.subnet.id
   }
 
+    /*
   service_principal {
     client_id     = var.CLIENT_ID
     client_secret = var.CLIENT_SECRET
   }
+    */
 
   role_based_access_control_enabled = true
 
