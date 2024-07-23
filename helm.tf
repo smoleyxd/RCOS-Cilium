506ac64a6d58e64e9fbc146db339ce193552344c
sbegin0 <sam.begin9@gmail.com>
2024-07-23 15:16:27 -0400

    fixed helm configurations and enabled prmetheus (WORKING)

--- helm.tf
+++ helm.tf
@@ -1,5 +1,3 @@
-# helm.tf
-
 provider "helm" {
   kubernetes {
     host                   = azurerm_kubernetes_cluster.default.kube_config[0].host
@@ -7,6 +5,11 @@ provider "helm" {
     client_key             = base64decode(azurerm_kubernetes_cluster.default.kube_config[0].client_key)
     cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.default.kube_config[0].cluster_ca_certificate)
   }
+}
+
+locals {
+  # Extract the hostname from the Kubernetes host URL
+  k8s_service_host = regex("https?://([^:]+)", azurerm_kubernetes_cluster.default.kube_config[0].host)[0]
 }
 
 resource "helm_release" "cilium" {
@@ -23,6 +26,16 @@ resource "helm_release" "cilium" {
 
   set {
     name  = "k8sServiceHost"
-    value = azurerm_kubernetes_cluster.default.kube_config[0].host
+    value = local.k8s_service_host
+  }
+
+  set {
+    name  = "prometheus.enabled"
+    value = "true"
+  }
+
+  set {
+    name  = "k8sServicePort"
+    value = "443"
   }
 }
