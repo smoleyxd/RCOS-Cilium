5a704d73b5cd62e101a666d7eb382fefb03801be
LevyLCH <132854847+LevyLCH@users.noreply.github.com>
2024-08-06 14:17:12 -0400

    Added helm folder with different charts

---
+++ helm/helmBandwidth.tf
@@ -0,0 +1,36 @@
+# helm.tf
+
+provider "helm" {
+  kubernetes {
+    host                   = azurerm_kubernetes_cluster.default.kube_config[0].host
+    client_certificate     = base64decode(azurerm_kubernetes_cluster.default.kube_config[0].client_certificate)
+    client_key             = base64decode(azurerm_kubernetes_cluster.default.kube_config[0].client_key)
+    cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.default.kube_config[0].cluster_ca_certificate)
+  }
+}
+
+resource "helm_release" "cilium" {
+  name       = "cilium"
+  repository = "https://helm.cilium.io/"
+  chart      = "cilium"
+  version    = "1.16.0" # specify the version you want to use
+  namespace  = "kube-system"
+
+
+  set {
+    name  = "k8sServiceHost"
+    value = azurerm_kubernetes_cluster.default.kube_config[0].host
+  }
+  
+  set {
+    name  = "kubeProxyReplacement"
+    value = "strict"
+  }
+
+  set {
+    name = "bandwidthManager.enabled"
+    value = "true"
+  }
+}
+
+
