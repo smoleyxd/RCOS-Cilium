4a759d6aa1026eb3742803784fc07b3be2e8a28b
LevyLCH <132854847+LevyLCH@users.noreply.github.com>
2024-08-06 13:44:05 -0400

    Update Tuning for Cilium in Helm

--- helm.tf
+++ helm.tf
@@ -13,7 +13,7 @@ resource "helm_release" "cilium" {
   name       = "cilium"
   repository = "https://helm.cilium.io/"
   chart      = "cilium"
-  version    = "1.15.7" # specify the version you want to use
+  version    = "1.16.0" # specify the version you want to use
   namespace  = "kube-system"
 
   set {
@@ -24,5 +24,99 @@ resource "helm_release" "cilium" {
   set {
     name  = "k8sServiceHost"
     value = azurerm_kubernetes_cluster.default.kube_config[0].host
+  }
+}
+
+resource "helm_release" "cilium_netkit" {
+  name       = "cilium"
+  repository = "https://helm.cilium.io/"
+  chart      = "cilium"
+  version    = "1.16.0" # specify the version you want to use
+  namespace  = "kube-system"
+
+  set {
+    name  = "routingMode"
+    value = "native"
+  }
+
+  set {
+    name  = "bpf.datapathMode"
+    value = "netkit"
+  }
+
+  set {
+    name  = "bpf.masquerade"
+    value = "true"
+  }
+
+  set {
+    name  = "kubeProxyReplacement"
+    value = "true"
+  }
+}
+
+resource "helm_release" "cilium_BIGTCPIPv6" {
+  name       = "cilium"
+  repository = "https://helm.cilium.io/"
+  chart      = "cilium"
+  version    = "1.16.0" # specify the version you want to use
+  namespace  = "kube-system"
+
+  set {
+    name  = "routingMode"
+    value = "native"
+  }
+
+  set {
+    name  = "bpf.masquerade"
+    value = "true"
+  }
+  
+  set {
+    name  = "ipv6.enabled"
+    value = "true"
+  }
+
+  set {
+    name  = "enableIPv6BIGTCP"
+    value = "true"
+  }
+
+  set {
+    name  = "kubeProxyReplacement"
+    value = "true"
+  }
+}
+
+resource "helm_release" "cilium_BIGTCPIPv4" {
+  name       = "cilium"
+  repository = "https://helm.cilium.io/"
+  chart      = "cilium"
+  version    = "1.16.0" # specify the version you want to use
+  namespace  = "kube-system"
+
+  set {
+    name  = "routingMode"
+    value = "native"
+  }
+
+  set {
+    name  = "bpf.masquerade"
+    value = "true"
+  }
+  
+  set {
+    name  = "ipv4.enabled"
+    value = "true"
+  }
+
+  set {
+    name  = "enableIPv4BIGTCP"
+    value = "true"
+  }
+
+  set {
+    name  = "kubeProxyReplacement"
+    value = "true"
   }
 }
