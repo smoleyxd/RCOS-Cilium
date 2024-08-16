d5a4bb64d0e8fdba3fc833af540cd7dfec4eebf6
hzmann <hzitzmann@optonline.net>
2024-08-16 13:58:22 -0400

    main.tf example file I used for learning HCP terraform/kubernetes basics

---
+++ Heather-ex/main.tf
@@ -0,0 +1,34 @@
+#iniitialize HCP provider to authenticate and manage resources in HCP
+provider "hcp" {
+  client_id     = "<your-client-id>"
+  client_secret = "<your-client-secret>"
+}
+
+#initialize AWS provider- provides necessary infrastructure for kubernetes cluster
+provider "aws" {
+  region = "us-west-2"
+}
+
+#create a VPC on AWS- a virtual network dedicated to your AWS account
+#where AWS resources like EC2 instances can be launched
+module "vpc" {
+  source = "./vpc"  #reference to the VPC module (defined in `vpc.tf`)
+}
+
+module "eks" {
+  source = "./eks"  #reference to the EKS module (defined in `eks.tf`)
+}
+
+#outputs used to expose certain values after infrastructure is deployed
+output "kubeconfig" {
+  value = module.eks.kubeconfig
+}
+
+output "cluster_name" {
+  value = module.eks.cluster_name #retrieve name of cluster specified in eks module
+}
+
+output "cluster_certificate" {
+  value = module.eks.cluster_certificate #provides certificate authority data needed to securley communicate with kubernetes clusters;
+  #fetches CA certificate from eks module, required for validating identify of kubernetes API server
+}
